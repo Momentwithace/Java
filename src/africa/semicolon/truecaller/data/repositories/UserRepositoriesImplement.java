0dc6951209b5e7ea2b34a0fab9e3d6ac13b2c38f
momentwithace <augustineezekiel763@gmail.com>
2022-07-18 21:25:01 +0100

    CMS

--- src/africa/semicolon/truecaller/data/repositories/UserRepositoriesImplement.java
+++ src/africa/semicolon/truecaller/data/repositories/UserRepositoriesImplement.java
@@ -1,13 +1,31 @@
 package africa.semicolon.truecaller.data.repositories;
 
+import africa.semicolon.truecaller.data.models.Contact;
 import africa.semicolon.truecaller.data.models.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class UserRespositoriesimplement implements UserRepostoories{
+public class UserRespositoriesimplement implements UserRepositories {
+    private int counter;
+    private List<User> users = new ArrayList<>();
     @Override
     public User save(User user) {
-        return null;
+        var userId = user.getId();
+        for (User value : users) {
+            if (value.getId() == userId) {
+                user.setFirstName(user.getFirstName());
+                user.setSecondName(user.getSecondName());
+                user.setEmail(user.getEmail());
+                user.setPhoneNumber(user.getPhoneNumber());
+                return value;
+            }
+        }
+        counter++;
+        user.setId(counter);
+        users.add(user);
+        return user;
+
     }
 
     @Override
---
+++ test/africa/semicolon/truecaller/data/repositories/UserRepositoriesImplementTest.java
@@ -0,0 +1,4 @@
+package africa.semicolon.truecaller.data.repositories;
+
+public class UserRepositoriesImplementTest {
+}
